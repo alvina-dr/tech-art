a49c162beb3b5a1bebf669a10492bb0506558dd9
DrAlvina24 <alvinadr24@gmail.com>
2020-12-09 15:24:13 +0100

    menu burger 1.1

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,12 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
+var menuOuvert = document.getElementById('m_ouvert')
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen = true;
+        menuOuvert.style.display='flex';
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
+        menuOuvert.style.display='none';
     }
 });
 
