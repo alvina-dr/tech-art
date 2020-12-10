a63dcb32cd09f64e7d0a6d0f8cafbe943de8154c
Candice Fradet <onc.fradet@gmail.com>
2020-12-10 17:47:38 +0100

    Fix captcha bug

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -1,27 +1,20 @@
+const openButton = document.querySelector("#open-captcha-button");
+// const closeButton = document.querySelector('#close-captcha-button');
+// const captcha = document.getElementById('captcha');
+// const verifier = document.querySelector('.purplebox');
 
+openButton.addEventListener("click", () => {
+  document.querySelector("#captcha").classList.remove('hidden');
+});
 
+// function fermerPopup(){
+//   captcha.style.display="none"
+// }
 
+// verifier.addEventListener("click",fermerPopup);
 
+// closeButton.addEventListener("click",fermerPopup);
 
-
-
-
-
-
-const openButton = document.getElementById('open-captcha-button');
-const closeButton = document.getElementById('close-captcha-button');
-const captcha = document.getElementById('captcha');
-const verifier = document.querySelector('.purplebox');
-
-function fermerPopup(){
-  captcha.style.display="none"
-}
-
-verifier.addEventListener("click",fermerPopup);
-
-closeButton.addEventListener("click",fermerPopup);
-
-openButton.addEventListener("click",function(){
-  console.log("openButtonClicked")
-})
-
+// openButton.addEventListener("click",function(){
+//   console.log("openButtonClicked")
+// })
