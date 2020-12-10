0b94b39781b1d38ef60e9bb644d4cb9fd4503e86
mthibault006 <thibault.maxime@outlook.com>
2020-12-10 16:53:25 +0100

    videoplayer play pause buttons

--- assets/js/videoplayer.js
+++ assets/js/videoplayer.js
@@ -1,11 +1,38 @@
 const playButton = document.querySelector('#play-video-button');
 const video = document.querySelector('#video');
+const controls = document.querySelector('.controls')
+
+//Partie en cours de conception
+
+const playBtn = document.querySelector('.play');
+const pauseBtn = document.querySelector('.pause');
+
+var timerWrapper = document.querySelector('.timer');
+var timer = document.querySelector('.timer span');
+var timerBar = document.querySelector('.timer div');
+
+//Fin partie en cours de conception
 
 playButton.addEventListener('click', function() {
-  video.play();
-  playButton.style.display = 'none';   
+    video.play();
+    playButton.style.display = 'none';
+    controls.style.display = 'flex';
 });
 
 video.addEventListener('ended', function() {
-  playButton.style.display = 'block';   
+    controls.style.display = 'none';
+    playButton.style.display = 'block';   
 });
+
+pauseBtn.addEventListener('click', function() {
+    video.pause();
+    pauseBtn.style.display = 'none';
+    playBtn.style.display = 'block';
+});
+
+playBtn.addEventListener('click', function() {
+    video.play();
+    playBtn.style.display = 'none';
+    pauseBtn.style.display = 'block';
+});
+//Partie en cours de conception
