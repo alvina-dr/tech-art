397d2faefb7e7df3d1196ca186b47801b81cfd34
DrAlvina24 <alvinadr24@gmail.com>
2020-12-11 09:57:31 +0100

    stop scroll menu

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -10,30 +10,36 @@ menuBtn.addEventListener('click', () => {
         menuBtn.classList.add('open');
         menuOpen = true;
         menuOuvert.style.display='flex';
+        document.documentElement.style.overflow = 'hidden';
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
         menuOuvert.style.display='none';
+        document.documentElement.style.overflow = 'auto';
     }
 });
 menuBtnQui.addEventListener('click', () => {
     menuOpen = false;
     menuOuvert.style.display='none';
     menuBtn.classList.remove('open');
+    document.documentElement.style.overflow = 'auto';
 });
 menuBtnOffre.addEventListener('click', () => {
     menuOpen = false;
     menuOuvert.style.display='none';
     menuBtn.classList.remove('open');
+    document.documentElement.style.overflow = 'auto';
 });
 menuBtnService.addEventListener('click', () => {
     menuOpen = false;
     menuOuvert.style.display='none';
     menuBtn.classList.remove('open');
+    document.documentElement.style.overflow = 'auto';
 });
 menuBtnProjet.addEventListener('click', () => {
     menuOpen = false;
     menuOuvert.style.display='none';
     menuBtn.classList.remove('open');
+    document.documentElement.style.overflow = 'auto';
 });
 const menuBtnBurger = document.querySelector('.menu-btn__burger');
